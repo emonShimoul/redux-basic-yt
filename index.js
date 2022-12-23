05f1a0af1214138f256d893cbeb57dcbb4718725
emonShimoul <emonshimoul@gmail.com>
2022-12-24 00:12:34 +0600

    define the actions

--- index.js
+++ index.js
@@ -3,6 +3,29 @@
 // reducer
 // store
 
+// CONSTANTS
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
+
 const initialState = {
     count: 0
+}
+
+const incrementCounterAction = () => {
+    return {
+        type: INCREMENT,
+    }
+}
+
+const decrementCounterAction = () => {
+    return {
+        type: DECREMENT,
+    }
+}
+
+const resetCounterAction = () => {
+    return {
+        type: RESET,
+    }
 }
