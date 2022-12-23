4e22abe35878d7bbbed261e26e82c9c2783aa402
emonShimoul <emonshimoul@gmail.com>
2022-12-24 00:07:19 +0600

    reset the counter project and initialize state

--- index.js
+++ index.js
@@ -1,78 +1,8 @@
-const { createStore } = require("redux");
+// state - count:0
+// action - increment, decrement, reset
+// reducer
+// store
 
-// defining constants
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-const ADD_USER = 'ADD_USER';
-
-// state
-const initialCounterState = {
+const initialState = {
     count: 0
 }
-
-const initialUsersState = {
-    users: [
-        { name: 'emon' }
-    ]
-}
-
-// action - Object - type, payload
-const incrementCounterAction = () => {
-    return {
-        type: INCREMENT,
-
-    }
-};
-
-const decrementCounterAction = () => {
-    return {
-        type: DECREMENT,
-
-    }
-};
-
-const addUser = () => {
-    return {
-        type: ADD_USER,
-        payload: { name: 'shimul' }
-
-    }
-};
-
-// create reducer for counter
-const counterReducer = (state = initialCounterState, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return {
-                ...state,
-                count: state.count + 1,
-            };
-        case DECREMENT:
-            return {
-                ...state,
-                count: state.count - 1,
-            };
-        default:
-            state;
-    }
-}
-
-// create store
-const store = createStore(counterReducer);
-
-store.subscribe(() => {
-    console.log(store.getState());
-})
-
-// dispatch action
-store.dispatch(incrementCounterAction())
-store.dispatch(incrementCounterAction())
-store.dispatch(incrementCounterAction())
-store.dispatch(decrementCounterAction())
-
-/*
-1. state
-2. dispatch action
-3. reducer
-4. store - getState(), dispatch(), subscribe()
-*/
