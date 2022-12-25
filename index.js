444fc6f52a287115754e9eab4c7c17d1162793a4
emonShimoul <emonshimoul@gmail.com>
2022-12-25 12:43:41 +0600

    add another reducer

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const { createStore } = require("redux");
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCT = "ADD_PRODUCT";
 
+// cart constants
+const GET_CART_ITEMS = "GET_CART_ITEMS";
+const ADD_CART_ITEM = "ADD_CART_ITEM";
+
 // product states
 const initialProductState = {
     products: ["sugar", "salt"],
     numOfProducts: 2
+};
+
+// cart states
+const initialCartState = {
+    cart: ["sugar"],
+    numOfProducts: 1,
 };
 
 // product actions
@@ -20,7 +30,21 @@ const getProducts = () => {
 const addProduct = (product) => {
     return {
         type: ADD_PRODUCT,
-        payload: product
+        payload: product,
+    };
+};
+
+// cart actions
+const getCart = () => {
+    return {
+        type: GET_CART_ITEMS
+    }
+}
+
+const addCart = (product) => {
+    return {
+        type: ADD_CART_ITEM,
+        payload: product,
     };
 };
 
@@ -42,12 +66,32 @@ const productReducer = (state = initialProductState, action) => {
 }
 
 // cartReducer
+const cartReducer = (state = initialCartState, action) => {
+    switch (action.type) {
+        case GET_CART_ITEMS:
+            return {
+                ...state
+            };
+        case ADD_CART_ITEM:
+            return {
+                cart: [...state.cart, action.payload],
+                numOfProducts: state.numOfProducts + 1
+            };
+        default:
+            state;
+    }
+}
+
+// cartReducer
 
 // store
-const store = createStore(productReducer);
+const store = createStore(cartReducer);
 store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch(getProducts());
-store.dispatch(addProduct('pen'));
+// store.dispatch(getProducts());
+// store.dispatch(addProduct('pen'));
+
+store.dispatch(getCart());
+store.dispatch(addCart('pen'));
