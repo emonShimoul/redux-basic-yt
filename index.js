b06337601c09c1b18be49b42c7548d83625a965e
emonShimoul <emonshimoul@gmail.com>
2022-12-25 21:52:00 +0600

    redux-logger middleware backup and start processing for redux-thunk middleware

--- index.js
+++ index.js
@@ -1,52 +1,12 @@
-const { createStore, applyMiddleware } = require("redux");
-const { default: logger } = require("redux-logger");
+// async actions - api calling
+// api url - https://jsonplaceholder.typicode.com/todos
+// middleware - redux-thunk
+// axios api
 
-// product constants
-const GET_PRODUCTS = "GET_PRODUCTS";
-const ADD_PRODUCT = "ADD_PRODUCT";
+// states
 
-// product states
-const initialProductState = {
-    products: ["sugar", "salt"],
-    numOfProducts: 2
-};
+// actions
 
-// product actions
-const getProducts = () => {
-    return {
-        type: GET_PRODUCTS
-    }
-}
-
-const addProduct = (product) => {
-    return {
-        type: ADD_PRODUCT,
-        payload: product,
-    };
-};
-
-// productReducer
-const productReducer = (state = initialProductState, action) => {
-    switch (action.type) {
-        case GET_PRODUCTS:
-            return {
-                ...state
-            };
-        case ADD_PRODUCT:
-            return {
-                products: [...state.products, action.payload],
-                numOfProducts: state.numOfProducts + 1
-            };
-        default:
-            return state;
-    }
-}
+// reduces
 
 // store
-const store = createStore(productReducer, applyMiddleware(logger));
-store.subscribe(() => {
-    console.log(store.getState());
-});
-
-store.dispatch(getProducts());
-store.dispatch(addProduct('pen'));
