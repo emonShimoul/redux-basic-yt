518923d55b30d4cd924423dd0d0aaa5a302640a8
emonShimoul <emonshimoul@gmail.com>
2022-12-26 19:39:21 +0600

    declare actions, reducer and store

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // middleware - redux-thunk
 // axios api
 
+const { createStore, applyMiddleware } = require("redux");
+const { default: thunk } = require("redux-thunk");
+
 // constants
 const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
 const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
@@ -14,3 +17,57 @@ const initialTodoStates = {
     isLoading: false,
     error: null,
 }
+
+// actions
+const getTodosRequest = () => {
+    return {
+        type: GET_TODOS_REQUEST
+    };
+};
+
+const getTodosSuccess = (todos) => {
+    return {
+        type: GET_TODOS_SUCCESS,
+        payload: todos
+    };
+};
+
+const getTodosFailure = (error) => {
+    return {
+        type: GET_TODOS_FAILURE,
+        payload: error
+    };
+};
+
+// reducers
+const todosReducer = (state = initialTodoStates, action) => {
+    switch (action.type) {
+        case GET_TODOS_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+            }
+        case GET_TODOS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                todos: action.payload,
+            }
+        case GET_TODOS_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            }
+        default:
+            break;
+    }
+}
+
+// store
+const store = createStore(todosReducer, applyMiddleware(thunk));
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(getTodosFailure());
