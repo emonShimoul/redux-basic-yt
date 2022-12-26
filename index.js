2a6ce9fc6d6675a5afdd5f5bbba8801fdf50f6c5
emonShimoul <emonshimoul@gmail.com>
2022-12-26 19:26:02 +0600

    declare states and constants

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 // middleware - redux-thunk
 // axios api
 
+// constants
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+const GET_TODOS_FAILURE = "GET_TODOS_FAILURE";
+
+// states
+const initialTodoStates = {
+    todos: [],
+    isLoading: false,
+    error: null,
+}
