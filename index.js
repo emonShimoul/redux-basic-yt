aba8515b7bd6037150da7959e12d55065861f157
emonShimoul <emonshimoul@gmail.com>
2022-12-26 19:54:49 +0600

    use axios to fetch data

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 // middleware - redux-thunk
 // axios api
 
+const { default: axios } = require("axios");
 const { createStore, applyMiddleware } = require("redux");
-const { default: thunk } = require("redux-thunk");
+const thunk = require("redux-thunk").default;
 
 // constants
 const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
 const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
 const GET_TODOS_FAILURE = "GET_TODOS_FAILURE";
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 // states
 const initialTodoStates = {
@@ -64,10 +66,27 @@ const todosReducer = (state = initialTodoStates, action) => {
     }
 }
 
+// async action creator
+const fetchData = () => {
+    return (dispatch) => {
+        dispatch(getTodosRequest());
+        axios.get(API_URL)
+            .then(res => {
+                const todos = res.data;
+                const titles = todos.map(todo => todo.title);
+                dispatch(getTodosSuccess(titles));
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+                dispatch(getTodosFailure(errorMessage));
+            })
+    }
+}
+
 // store
 const store = createStore(todosReducer, applyMiddleware(thunk));
 store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch(getTodosFailure());
+store.dispatch(fetchData());
